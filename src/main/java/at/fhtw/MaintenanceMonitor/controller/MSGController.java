af3a561b2e99a41ea40d0e4e1ba403ed647e7dba
Alexander Filipovic <ic21b020@technikum-wien.at>
2022-11-20 16:19:26 +0100

    Implementierung einer GET-Anfrage für das Monitoring #5

--- src/main/java/at/fhtw/MaintenanceMonitor/controller/MSGController.java
+++ src/main/java/at/fhtw/MaintenanceMonitor/controller/MSGController.java
@@ -1,9 +1,20 @@
 package at.fhtw.MaintenanceMonitor.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 public class MSGController {
+
     public static String motd = "Default Message";
+
+    /**
+     * Rest Methode um die "Message of the day" abfragen mit einem GET Request
+     * @return
+     */
+    @GetMapping("/monitor")
+    public String getmsg(){
+        return motd;
+    }
 
     /**
      * Rest Methode um die "Message of the day" zu reseten mit einem DELETE request
