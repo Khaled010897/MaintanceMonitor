e62f053e7446e727649f8a4c27bc6430128de32a
Erwin Li <ic19b005@technikum-wien.at>
2022-11-23 18:20:08 +0100

    Die motd soll veränderbar sein #11

--- src/main/java/at/fhtw/MaintenanceMonitor/controller/MSGController.java
+++ src/main/java/at/fhtw/MaintenanceMonitor/controller/MSGController.java
@@ -1,9 +1,21 @@
 package at.fhtw.MaintenanceMonitor.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public class MSGController {
     public static String motd = "Default Message";
+
+    /**
+     * Rest Methode um die "Message of the day" abfragen mit einem POST request
+     * @return
+     */
+    @PutMapping("/monitor")
+    public static String setmsg(@RequestBody String msg){
+        motd = msg;
+        return motd;
+    }
 
     /**
      * Rest Methode um die "Message of the day" zu reseten mit einem DELETE request
